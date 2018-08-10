8a100686744cf53f6465cbd30b0c376966aa61fc
Yinyin L <yinyinl@gmail.com>
2018-08-11 01:35:47 +0800

    perform stdio redirect with exported function

--- src/processfork.c
+++ src/processfork.c
@@ -9,6 +9,7 @@
 #include "recordlogs/recordlogs.h"
 #include "signalhandling/signalhandling.h"
 
+#include "processfork/changeprocessproperty.h"
 #include "processfork/closenonstdiofd.h"
 
 static pid_t fork_for_daemonize(int fork_stage) {
@@ -64,47 +65,6 @@ static pid_t fork_for_exec() {
     return ret_id;
 }
 
-static int redirect_stdio_fds(int fd_stdin, int fd_stdout, int fd_stderr) {
-    int retcode = -1;
-    do {
-        if (-1 == fd_stdin) {
-            if (-1 == (fd_stdin = open("/dev/null", O_RDONLY))) {
-                RECORD_ERROR(LOG_ERR, "failed on open /dev/null for STDIN");
-                break;
-            }
-        }
-        retcode = -2;
-        if (-1 == dup2(fd_stdin, STDIN_FILENO)) {
-            RECORD_ERROR(LOG_ERR, "failed on override STDIN.");
-            break;
-        }
-        retcode = -3;
-        if (-1 != fd_stdout) {
-            if (-1 == dup2(fd_stdout, STDOUT_FILENO)) {
-                RECORD_ERROR(LOG_ERR, "failed on override STDOUT.");
-                break;
-            }
-        }
-        if (-1 != fd_stderr) {
-            if (-1 == dup2(fd_stderr, STDERR_FILENO)) {
-                RECORD_ERROR(LOG_ERR, "failed on override STDERR.");
-                break;
-            }
-        }
-        retcode = 0;
-    } while (0);
-    if (-1 != fd_stdin) {
-        close(fd_stdin);
-    }
-    if (-1 != fd_stdout) {
-        close(fd_stdout);
-    }
-    if (-1 != fd_stderr) {
-        close(fd_stderr);
-    }
-    return retcode;
-}
-
 pid_t execve_fork(int fd_stdin,
                   int fd_stdout,
                   int fd_stderr,
@@ -115,7 +75,7 @@ pid_t execve_fork(int fd_stdin,
     if ((0 < ret_id) || (-1 == ret_id)) {
         return ret_id;
     }
-    redirect_stdio_fds(fd_stdin, fd_stdout, fd_stderr);
+    processfork_redirect_stdio_fd(fd_stdin, fd_stdout, fd_stderr);
     close_nonstdio_fd();
     execve(cmd_argv[0], cmd_argv, cmd_envp);
     exit(126);
